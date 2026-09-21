57cc5cce76a10012deef1e9029737a63f57c52b8
Saydia <sadiatasnim646@gmail.com>
2026-09-21 18:14:44 +0600

    wordFrequencyCount

--- titleCaseSentence.js
+++ titleCaseSentence.js
@@ -25,6 +25,7 @@ function titleCaseSentence(str) {
         .toLowerCase()
         .trim()
         .split(/\s+/)
+        .filter(word => word.length > 0)
         .map(word => word[0].toUpperCase() + word.slice(1))
         .join(" ");
 }
